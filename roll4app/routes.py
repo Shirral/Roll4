94242dc8cde7889c5252328749eef16f86496a2c
Shirral <132143164+Shirral@users.noreply.github.com>
2024-09-19 15:36:47 +0100

    Add and route register.html and userprofile.html
    Add user registration feature and make sure the information from the SQL database is retrieved correctly.

--- roll4app/routes.py
+++ roll4app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from roll4app import app, mongo, db
 from roll4app.models import Users
 
@@ -7,3 +7,19 @@ from roll4app.models import Users
 def list_view():
     lists = mongo.db.Lists.find()
     return render_template("lists.html", lists=lists)
+
+@app.route("/userprofile")
+def userprofile():
+    user = list(Users.query.all())
+    return render_template("userprofile.html", user=user)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = Users(
+            user_name=request.form.get("user_name"),
+            user_password=request.form.get("user_password"))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("userprofile"))
+    return render_template("register.html")
