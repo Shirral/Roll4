abd9270b7b2bb2fd2e024d36d818dea92dbf58d0
Shirral <132143164+Shirral@users.noreply.github.com>
2024-09-20 10:34:47 +0100

    Update the register function; add flash messages; update styling

--- roll4app/routes.py
+++ roll4app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
 from roll4app import app, mongo, db
 from roll4app.models import Users
 
@@ -16,11 +17,20 @@ def userprofile():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        existing_user = Users.query.filter_by(user_name=request.form.get("user_name").lower()).first()
+
+        if existing_user:
+            flash("Username already taken. Sorry!") #change flash to sth nicer later
+            return redirect(url_for("register"))
+
         user = Users(
-            user_name=request.form.get("user_name"),
-            user_password=request.form.get("user_password"))
+            user_name=request.form.get("user_name").lower(),
+            user_password=generate_password_hash(request.form.get("user_password")))
         db.session.add(user)
         db.session.commit()
+
+        session["currentuser"] = request.form.get("user_name").lower()
+        flash(f"Welcome onboard, {session['currentuser']}!")
         return redirect(url_for("userprofile"))
     return render_template("register.html")
 
@@ -29,7 +39,7 @@ def login():
     # if request.method == "POST":
     #     user = Users.query.filter_by(user_name=request.form.get("user_name").lower()).first()
 
-    #     if user == True:
+    #     if user:
     #         if user.user_password == request.form.get("user_password"):
 
     
