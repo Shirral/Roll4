1054f87d6828a7023ac1064bfe74430b34f47ec2
Shirral <132143164+Shirral@users.noreply.github.com>
2024-09-23 17:36:21 +0100

    Add "notes" section to each list item on the the addlist.html form
    The section is hidden by default as adding notes is optional; it is displayed once the user clicks on the button tied to it. Update the script link to allow the jQuery animations.

--- roll4app/static/js/script.js
+++ roll4app/static/js/script.js
@@ -1,4 +1,22 @@
 //initialize sidenav
 $(document).ready(function () {
     $('.sidenav').sidenav();
+    $('.tooltipped').tooltip();
 });
+
+$( ".notesbtn" ).on( "click", function() {
+    
+    let divs = document.getElementsByClassName("notesbtn");
+    let len = divs.length;
+    let num;
+    console.log(num);
+    for (var i = 1; i <= len; i++){
+        if (this.id == "notesbtn"+i){
+            num = i;
+            break;
+        } 
+    }
+    console.log(num);
+
+    $( "#notes"+num ).toggle("slow");
+  });
