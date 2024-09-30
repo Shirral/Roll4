cf844c935a4f102f38a6f8654294b720334bc97a
Shirral <132143164+Shirral@users.noreply.github.com>
2024-09-30 16:13:01 +0100

    Update  __init__.py
    Prepare the Heroku deployment for connecting to an external database.

--- roll4app/__init__.py
+++ roll4app/__init__.py
@@ -2,21 +2,23 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
-# if os.path.exists(".env"):
 from dotenv import load_dotenv
 load_dotenv()
-
-# Debugging - to delete later
-# print("MONGO_URI:", os.environ.get("MONGO_URI"))
-# print("DB_URL:", os.environ.get("DB_URL"))
 
 app = Flask(__name__)
 
 # Configuration
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.secret_key = os.environ.get("SECRET_KEY")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
 
 # Initialize MongoDB and SQLAlchemy
 mongo = PyMongo(app)
