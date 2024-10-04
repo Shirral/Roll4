edac10c5494566729fdf80ff7c1007fcc549e07e
Shirral <132143164+Shirral@users.noreply.github.com>
2024-10-04 13:59:16 +0100

    Update debug to False

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from roll4app import app
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=os.environ.get("PORT"),
-            debug=True)
+            debug=False)
